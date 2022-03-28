36e09777250a2f9263e0a1472200b2fe79bac1cf
kamilovkz <001kamilov@gmail.com>
2022-03-28 15:51:47 +0600

    Release v1

---
+++ main.py
@@ -0,0 +1,31 @@
+import os, shutil
+import zipfile
+import datetime
+import yadisk
+
+def search():
+    poisk = []
+    with zipfile.ZipFile('{}.zip'.format(str(datetime.datetime.now()).replace("-","").replace('.', '').replace(':','')), 'w') as archive:
+        for address, dirs, files in os.walk(input('Enter way: ')):
+            for file in files:
+                #Условие дубликатов
+                file_name = file.split('\\')[-1]
+                count = 1
+                for f in poisk:
+                    if f == file_name:
+                        if f'({count - 1})' in file_name:
+                            file_name = file_name.replace(f'({count - 1})', '')
+                        file_name = f'({count}).'.join(file_name.split('.'))
+                        count += 1
+                #Условие файлов
+                if file_name.endswith(".docx") or file_name.endswith(".doc") or file_name.endswith(".pdf"):
+                    poisk.append(file_name)
+                    archive.write(os.path.join(address,file), file_name)
+                    print(file_name, ' is copied')
+    print('Archive is completed!')
+    yandex = yadisk.YaDisk(token='AQAAAAAimz9pAAfHt_DHHitN_UCeiX0Fy2N3WN4')
+    yandex.upload(archive.filename, '/' + archive.filename)
+    print('Archive successfully sent')
+    os.remove(archive.filename)
+    print('Archive successfully deleted from folder')
+search()

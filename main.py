ef13fbadfaf9e4888bc7db417d656185239d4589
Nail Kamilov <38252272+Kamilovkz@users.noreply.github.com>
2022-12-22 01:23:12 +0600

    Update main.py
    Update script

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def search():
     with zipfile.ZipFile('{}.zip'.format(str(datetime.datetime.now()).replace("-","").replace('.', '').replace(':','')), 'w') as archive:
         for address, dirs, files in os.walk(input('Enter way: ')):
             for file in files:
-                #Условие дубликатов
+                #Solving problem with dublicates
                 file_name = file.split('\\')[-1]
                 count = 1
                 for f in poisk:
@@ -20,15 +20,17 @@ def search():
                             file_name = file_name.replace(f'({count - 1})', '')
                         file_name = f'({count}).'.join(file_name.split('.'))
                         count += 1
-                #Условие файлов
+                #File extensions
                 if file_name.endswith(".docx") or file_name.endswith(".doc") or file_name.endswith(".pdf"):
                     poisk.append(file_name)
                     archive.write(os.path.join(address,file), file_name)
                     print(file_name, ' is copied')
-    print('Archive is completed!')
+    print('Archive is created!')
+    # Send atchive to Yandex cloud using yadisc library
     yandex = yadisk.YaDisk(token='PASTHEREYOURAPI')
     yandex.upload(archive.filename, '/' + archive.filename)
     print('Archive successfully sent')
+    # Delete archive
     os.remove(archive.filename)
     print('Archive successfully deleted from folder')
 

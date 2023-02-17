5f7c071b52419e195fdfdcf5d518dcadbb0dd3d1
Kamilovkz <001kamilov@gmail.com>
2023-02-17 20:09:42 +0600

    Update script

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import os, shutil
+import os
 import zipfile
 import datetime
 import yadisk
 
 def main():
-    seacrh()
+    run = Search()
+    run.engine()
 
-def search():
-    poisk = []
-    with zipfile.ZipFile('{}.zip'.format(str(datetime.datetime.now()).replace("-","").replace('.', '').replace(':','')), 'w') as archive:
-        for address, dirs, files in os.walk(input('Enter way: ')):
+import os
+import datetime
+import zipfile
+import yadisk
+
+def main():
+    search = Search(input_dir='/path/to/search/directory', yadisk_token='PASTHEREYOURAPI')
+    search.create_archive()
+
+class Search:
+    def __init__(self, input_dir, yadisk_token):
+        self.input_dir = input_dir
+        self.yadisk_token = yadisk_token
+
+    def search_files(self):
+        poisk = []
+        for address, dirs, files in os.walk(self.input_dir):
             for file in files:
-                #Solving problem with dublicates
+                #Solving problem with duplicates
                 file_name = file.split('\\')[-1]
                 count = 1
                 for f in poisk:
@@ -23,16 +37,23 @@ def search():
                 #File extensions
                 if file_name.endswith(".docx") or file_name.endswith(".doc") or file_name.endswith(".pdf"):
                     poisk.append(file_name)
-                    archive.write(os.path.join(address,file), file_name)
-                    print(file_name, ' is copied')
-    print('Archive is created!')
-    # Send atchive to Yandex cloud using yadisc library
-    yandex = yadisk.YaDisk(token='PASTHEREYOURAPI')
-    yandex.upload(archive.filename, '/' + archive.filename)
-    print('Archive successfully sent')
-    # Delete archive
-    os.remove(archive.filename)
-    print('Archive successfully deleted from folder')
+                    yield os.path.join(address, file), file_name
+
+    def create_archive(self):
+        archive_name = '{}.zip'.format(str(datetime.datetime.now()).replace("-","").replace('.', '').replace(':',''))
+        with zipfile.ZipFile(archive_name, 'w') as archive:
+            for file_path, file_name in self.search_files():
+                archive.write(file_path, file_name)
+                print(file_name, ' is copied')
+        print('Archive is created!')
+        # Send archive to Yandex cloud using yadisk library
+        yandex = yadisk.YaDisk(token=self.yadisk_token)
+        yandex.upload(archive_name, '/' + archive_name)
+        print('Archive successfully sent')
+        # Delete archive
+        os.remove(archive_name)
+        print('Archive successfully deleted from folder')
+
 
 if __name__ == "__main__":
     main()
